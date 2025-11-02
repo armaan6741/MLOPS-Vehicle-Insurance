3f03cb2163c634e5f1558d3c595863472c4ad940
Bot <levsthomas@gmail.com>
2025-11-02 22:12:52 +0530

    Model Created

--- demo.py
+++ demo.py
@@ -23,3 +23,27 @@ from src.pipline.training_pipeline import TrainPipeline
 
 pipline = TrainPipeline()
 pipline.run_pipeline()
+
+# import os
+# from datetime import datetime
+
+# # Base directory for artifacts
+# TRIAL_DIR = "trial"
+# SUB_TRIAL = "sub-trial"
+
+# # Create a timestamp string, e.g. "2025_11_02_14_45_30"
+# TIMESTAMP = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+# # Combine them into one path
+# trial_dir: str = os.path.join(TRIAL_DIR)
+
+# # Print the result
+# print("Trial directory path:", trial_dir)
+
+# # (Optional) Create the directory if it doesn’t exist
+# # os.makedirs(trial_dir, exist_ok=True)
+# # print("Directory created (if it didn’t already exist).")
+
+# sub_trial: str = os.path.join(trial_dir,SUB_TRIAL)
+# os.makedirs(sub_trial, exist_ok=True)
+
